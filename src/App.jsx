4c3bde4edac874ddf66c5094b02a7d13bb1fc4af
victortonneberg <84105735+victortoneberg@users.noreply.github.com>
2025-04-04 15:17:29 +0200

    Variabler i scss

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg"
 // import "./App.css"
 import ProfilePage from "./components/ProfilePage"
 import Header from "./components/Header"
+import Layout from "./components/Layout"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Header />
+    <Layout />
+    // <Header />
     // <ProfilePage />
 
     // <>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,11 @@
+import Header from "./Header"
+import ProfilePage from "./ProfilePage"
+
 export default function Layout() {
-  return
+  return (
+    <>
+      <Header />
+      <ProfilePage />
+    </>
+  )
 }
